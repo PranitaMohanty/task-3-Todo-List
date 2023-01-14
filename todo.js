4bf532169b46fc885cee0727f03652930c510a7c
PranitaMohanty <116963764+PranitaMohanty@users.noreply.github.com>
2023-01-14 19:28:18 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,26 @@
+const addForm=document.querySelector('.add');
+const list=document.querySelector('.todos');
+addForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const todo=addForm.add.value.trim();
+    if(todo.length !=0){
+        generateTemplate(todo);
+        addForm.requestFullscreen();
+    }else{
+        alert('Please input task');
+    }
+});
+const generateTemplate=(todo)=>
+{
+    const html= <li class='list-group-item d-flex justify-content-between align-items-center'>
+    <span>${todo}</span>
+    <i class="fa fa-trash-o delete"></i>
+    </li>;
+    list.innerHTML +=html;
+}
+list.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('delete'))
+    {
+        e.target.parentElement.remove();
+    }
+})
